837d206d018a51d9387df1ea11b9c3d7c93ab198
nebulon42 <nebulon42@yandex.com>
2016-01-25 23:03:45 +0100

    use path.join for OS independency

--- install.js
+++ install.js
@@ -5,7 +5,7 @@ var config ={};
 config.basedir = 'magnacarto';
 config.version = 'v0.1.0';
 config.repositoryUrl = 'https://github.com/gmgeo/libmagnacarto.git';
-config.repositoryDest = 'magnacarto/src/github.com/gmgeo/libmagnacarto';
+config.repositoryDest = path.join('magnacarto', 'src', 'github.com', 'gmgeo', 'libmagnacarto');
 
 if (!sh.which('git')) {
   sh.echo('This script requires git. Exiting.');
@@ -32,18 +32,18 @@ if (sh.exec('git clone --branch ' + config.version + ' ' + config.repositoryUrl 
 
 sh.cd(config.basedir);
 sh.env['GOPATH'] = sh.pwd();
-sh.env['PATH'] += path.delimiter + sh.pwd() + '/bin';
+sh.env['PATH'] += path.delimiter + path.join(sh.pwd(), 'bin');
 
 if (sh.exec('go get github.com/tools/godep').code !== 0) {
   sh.echo('Getting godep failed.');
   sh.exit(1);
 }
 
-sh.cd('src/github.com/gmgeo/libmagnacarto');
+sh.cd(path.join('src', 'github.com', 'gmgeo', 'libmagnacarto'));
 
 if (sh.exec('make').code !== 0) {
   sh.echo('Building library failed.');
   sh.exit(1);
 }
 
-sh.mv('build/libmagnacarto.*', '../../../../bin/');
+sh.mv(path.join('build', 'libmagnacarto.*'), path.join('..', '..', '..', '..', 'bin'));
